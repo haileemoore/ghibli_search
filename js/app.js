7c56113932f406621bdf0589dd389d6d0b748e6f
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-05-15 18:12:44 -0500

    displaying all data in console - need to get on page

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ $(() => { // START JQUERY
     event.preventDefault(); // PREVENT IMMEDIATE PAGE REFRESH
 
     let userInput = $('input[type="text"]').val()
+      // console.log(userInput);
 
     let link = `https://ghibliapi.herokuapp.com/${userInput}`
     let linkFilms = `https://ghibliapi.herokuapp.com/films`
@@ -16,6 +17,7 @@ $(() => { // START JQUERY
     let linkLocations = `https://ghibliapi.herokuapp.com/locations`
     let linkSpecies = `https://ghibliapi.herokuapp.com/species`
     let linkVehicles = `https://ghibliapi.herokuapp.com/vehicles`
+
 
     $.ajax(
       {
@@ -26,13 +28,25 @@ $(() => { // START JQUERY
     ).then(
         (data) => {
           // $('single-film-data').html(data.title);
-          $('films').html(linkFilms);
-          $('people').html(linkPeople);
-          $('locations').html(linkLocations);
-          $('species').html(linkSpecies);
-          $('vehicles').html(linkVehicles);
+          for (object of data) {
+                console.log(data);
+          }
+        const $data = data
+        // console.log(data);
 
-        console.log(data);
+          for (let i =0; i < userInput; i++) {
+            const $table = $('<table>')
+            const $th = $('<th>')
+            const $tr = $('<tr>')
+            const $td = $('films').html(data[i].films);
+            const $td1 = $('people').html(data[i].people);
+            const $td2 = ('locations').html(data[i].locations);
+            const $td3 = $('species').html(data[i].species);
+            const $td4 = $('vehicles').html(data[i].vehicles);
+            $table.append($td, $td1, $td2, $td3, $td4)
+            $th.append($td, $td1, $td2, $td3, $td4)
+
+          }
       },
       (error) => {
         console.log('Error: Ghibli not found');
