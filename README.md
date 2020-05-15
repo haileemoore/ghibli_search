01525f9edad64172e711e8fd8566a2652d4bf07c
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-05-15 15:27:54 -0500

    each link pulls data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # haileedoyle.github.io
 
+General Assembly Project 1
+
+API: https://ghibliapi.herokuapp.com/
---
+++ js/app.js
@@ -0,0 +1,45 @@
+// console.log('test');
+// API : https://ghibliapi.herokuapp.com
+
+
+$(() => { // START JQUERY
+
+  $('button').on('click', (event) => {
+
+    event.preventDefault(); // PREVENT IMMEDIATE PAGE REFRESH
+
+    let userInput = $('input[type="text"]').val()
+
+    let linkFilms = `https://ghibliapi.herokuapp.com/films`
+    let linkPeople = `https://ghibliapi.herokuapp.com/people`
+    let linkLocations = `https://ghibliapi.herokuapp.com/locations`
+    let linkSpecies = `https://ghibliapi.herokuapp.com/species`
+    let linkVehicles = `https://ghibliapi.herokuapp.com/vehicles`
+
+    $.ajax(
+      {
+        url: linkPeople,
+        type: 'GET',
+        data: userInput
+      }
+    ).then(
+        (data) => {
+          // $('single-film-data').html(data.title);
+          $('films').html(linkFilms);
+          $('people').html(linkPeople);
+          $('locations').html(linkLocations);
+          $('species').html(linkSpecies);
+          $('vehicles').html(linkVehicles);
+
+        console.log(data);
+      },
+      (error) => {
+        console.log('Error: Ghibli not found');
+      }
+    ) // END AJAX
+  }) // END BUTTON CLICK
+}) // END JQUERY
+
+// IF INPUT TYPE TEXT === TITLE, SHOW SINGLE FILM DATA SEARCHED BY TITLE
+// ELSE IF TEXT === FILMS, SHOW ALL GHIBLI FILMS
+// ELSE IF TEXT ===
