d8eb3c92008f2ae931233ab00b283529b0ee0145
Hailee Doyle <haileedoyle@Hailees-MacBook-Pro.local>
2020-05-16 19:35:08 -0500

    all data links to lists on page-need to format

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ $(() => { // START JQUERY
       // console.log(userInput);
 
     let link = `https://ghibliapi.herokuapp.com/${userInput}`
-    let linkFilms = `https://ghibliapi.herokuapp.com/films`
-    let linkPeople = `https://ghibliapi.herokuapp.com/people`
-    let linkLocations = `https://ghibliapi.herokuapp.com/locations`
-    let linkSpecies = `https://ghibliapi.herokuapp.com/species`
-    let linkVehicles = `https://ghibliapi.herokuapp.com/vehicles`
+    // let linkFilms = `https://ghibliapi.herokuapp.com/films`
+    // let linkPeople = `https://ghibliapi.herokuapp.com/people`
+    // let linkLocations = `https://ghibliapi.herokuapp.com/locations`
+    // let linkSpecies = `https://ghibliapi.herokuapp.com/species`
+    // let linkVehicles = `https://ghibliapi.herokuapp.com/vehicles`
 
 
     $.ajax(
@@ -29,16 +29,39 @@ $(() => { // START JQUERY
         (data) => {
           // $('single-film-data').html(data.title);
           for (object of data) {
-
+          // PUSH THE TITLES INTO li
             let title = object.title
-            // console.log(title); // RETURNS ALL TITLES OF FILMS
-            // PUSH THE TITLES INTO li
-            let people = object.linkPeople
-            console.log(people);
-          for (let i = 0; i < 1; i++) {
-            const $li = $('<li>').text(title)
-            $('.titles').append($li)
+            console.log(title); // RETURNS ALL TITLES OF FILMS
+            let people = object.name
+            // console.log(people);
+            let locations = object.name
+            // console.log(locations);
+            let species = object.name
+            // console.log(species);
+            let vehicles = object.name
 
+          for (let i = 0; i < 1; i++) {
+            const $liTitles = $('<li>').text(title)
+            const $liPeople = $('<li>').text(people)
+            const $liLocate = $('<li>').text(locations)
+            const $liSpecies = $('<li>').text(species)
+            const $liVehicles = $('<li>').text(vehicles)
+
+
+            // IF USERINPUT === FILMS
+            $('.titles').append($liTitles)
+
+            // ELSE IF === PEOPLE
+            $('.people').append($liPeople)
+
+            // ELSE IF === LOCATIONS
+            $('.locations').append($liLocate)
+
+            // ELSE IF === SPECIES
+            $('.species').append($liSpecies)
+
+            // ELSE IF === VEHICLES
+            $('.vehicles').append($liVehicles)
           }
         }
       },
